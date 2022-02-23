15ec09d48051d4af9dce269e560af2673fdddda6
naama-knoll <73070902+naama-knoll@users.noreply.github.com>
2022-02-23 21:32:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,24 +5,6 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-//        var hmCountry= new HashMap<String,String>();
-//        hmCountry.put("ES","Spain");
-//        hmCountry.put("DE","Germany");
-//        hmCountry.put("FR","France");
-//        hmCountry.put("CN","China");
-//        hmCountry.put("US","United States");
-//        hmCountry.put("RU","Russia");
-//        hmCountry.put("GB","United Kingdom");
-//        hmCountry.put("IN","India");
-//        hmCountry.put("BR","Brazil");
-//        hmCountry.put("SA","South Africa");
-//        hmCountry.put("IL","Israel");
-//
-//        int size=hmCountry.size();
-//        //System.out.println(size);
-//
-//        System.out.println(hmCountry);
-
         var p1=new Product(1,"soda");
         var p2=new Product(2,"kola");
         var p3=new Product(3,"water");
